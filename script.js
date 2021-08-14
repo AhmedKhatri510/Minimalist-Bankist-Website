61954656dd0416bdcfabbb3820ce61b46b86b3a6
AhmedKhatri510 <61470119+AhmedKhatri510@users.noreply.github.com>
2021-08-14 13:48:12 +0530

    added script at the header section with defer attribute to load the HTML and script in order

--- script.js
+++ script.js
@@ -536,3 +536,19 @@ console.log(h1.parentElement.children);
   }
 });
  */
+
+//lifecyle DOM event
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded!', e);
+});
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
